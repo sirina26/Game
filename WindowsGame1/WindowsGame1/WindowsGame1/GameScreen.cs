7193091e3fdfc87e9f4f225baf2068be19bae51e
sirine <sassi@intechinfo.fr>
2018-04-04 01:07:02 +0200

    Begin

---
+++ WindowsGame1/WindowsGame1/WindowsGame1/GameScreen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace WindowsGame1
+{
+    public class GameScreen
+    {
+        public virtual void Initialize() { }
+        public virtual void LoadContent(ContentManager Content);
+        public virtual void Update(GameTime gameTime) { }
+        public virtual void Draw(SpriteBatch spriteBatch) { }
+    }
+}
