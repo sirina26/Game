81673b5f61bea247bbf13398ec0abdd2a045b2e6
sirine <sassi@intechinfo.fr>
2018-04-04 12:16:20 +0200

    Try to fix git problem.

---
+++ PlayWithMac/PlayWithMac/GameScreen.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework.Content;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PlayWithMac
+{
+    public class GameScreen
+    {
+        public virtual void Initialize() { };
+        public virtual void LoadContent(ContentManager);
+    }
+}
