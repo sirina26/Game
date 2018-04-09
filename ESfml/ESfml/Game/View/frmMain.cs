551efc920bea7ea331b6f0cc096066f9e4f1bc0c
sirine <sassi@intechinfo.fr>
2018-04-09 11:22:26 +0200

    splash screen

---
+++ ESfml/ESfml/Game/View/frmMain.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace ESfml.Game.View
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            Thread th = new Thread(new ThreadStart(StartForm));
+            th.Start();
+            Thread.Sleep(5000);
+            InitializeComponent();
+            th.Abort();
+        }
+        public void StartForm()
+        {
+            Application.Run(new frmSplashScreen());
+        }
+    }
+}
--- ESfml/ESfml/Program.cs
+++ ESfml/ESfml/Program.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
+using ESfml.Game.View;
 
 namespace ESfml
 {
@@ -14,9 +16,18 @@ namespace ESfml
         [STAThread]
         static void Main()
         {
-            PlayWithMac pwm = new PlayWithMac();
-            pwm.Run();
-           
+
+            /* PlayWithMac pwm = new PlayWithMac();
+             pwm.Run();*/
+            Thread th = new Thread(new ThreadStart(StartForm));
+            th.Start();
+            Thread.Sleep(5000);
+            Application.Run(new frmSplashScreen());
+            th.Abort();
+        }
+        public static void StartForm()
+        {
+            Application.Run(new frmSplashScreen());
         }
     }
 }
