bb52cec33b842ff66f7efa37096149c11584bcb9
sirine <sassi@intechinfo.fr>
2018-06-06 09:12:33 +0200

    supprimer inutile using

--- ESfml/PlayWithMac.Model/Macron.cs
+++ ESfml/PlayWithMac.Model/Macron.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using SFML.Graphics;
 using SFML.System;
 using SFML.Window;
