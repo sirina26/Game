b0e03900642f5948910712e6d95b1e60fc022e4a
sirine <sassi@intechinfo.fr>
2018-06-28 10:59:28 +0200

    f

---
+++ ESfml/PlayWithMac.Model/GameOver.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+using SFML.Audio;
+
+namespace PlayWithMac.Model
+{
+    class GameOver
+    {
+        public void Draw(RenderWindow windowHandler, int xOffset, int yOffset)
+        {
+            RenderWindow windowGame2 = new RenderWindow(new VideoMode(1200, 700), "PlayWithMac");
+            Questions ques = new Questions(1200, 700);
+
+            while (windowGame2.IsOpen)
+            {
+                if (Keyboard.IsKeyPressed(Keyboard.Key.Escape))
+                {
+                    ques.Move(Keyboard.Key.Up);
+                }
+            }
+            ques.Draw(windowGame2);
+            windowGame2.Display();
+        }
+    }
+}
